fe2496683ad8e8fbe1b40ff0bff7dded10b91313
Aybike Celebi Visser <celebiaybike@gmail.com>
2026-05-03 20:03:46 +0200

    two additional server check

--- server.js
+++ server.js
@@ -30,13 +30,15 @@ server.listen(PORT, () => {
  -get a triplicity and all its signs // checked// http://localhost:5005/triplicities/:triplicityId?_embed=signs
  -get signs by duality and/or quadruplicity // duality checked// http://localhost:5005/signs/?duality=Masculine&triplicityId=fire1
    quadruplicity //checked // http://localhost:5005/signs/?quadruplicity=Cardinal&triplicityId=fire1
- -get all users //checked// http://localhost:5005/users
+ -get a single sign //checked// http://localhost:5005/signs/:signId
+   -get all users //checked// http://localhost:5005/users
+ -get a single user //checked// http://localhost:5005/users/:userId
  -get all favorites //checked// http://localhost:5005/favorites
  -get favorites by user //checked// http://localhost:5005/favorites?userId=ab111112
  -post -add favorite body,  userId signId //checked// http://localhost:5005/favorites
  -delete favorites //checked// http://localhost:5005/favorites/:favoriteId
  -get all comments //checked// http://localhost:5005/comments
- -get comments by sign //checke// http://localhost:5005/comments/?signId=1aries
+ -get comments by sign //checked// http://localhost:5005/comments/?signId=1aries
  -get comments by user //checked//http://localhost:5005/comments/?userId=ab111112
  -post create comment body, userId signId text //checked// http://localhost:5005/comments
  -patch edit comment // checked//http://localhost:5005/comments/:commentId
