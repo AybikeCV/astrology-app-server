a309d9d03b0cbd4913f591f03c8b40bfc83d2c62
Aybike Celebi Visser <celebiaybike@gmail.com>
2026-05-05 20:40:14 +0200

    final db-server checked again

--- server.js
+++ server.js
@@ -28,19 +28,19 @@ server.listen(PORT, () => {
  -get all triplicities // checked //runs finally // http://localhost:5005/triplicities 
  -get all signs// checked // http://localhost:5005/signs
  -get a triplicity and all its signs // checked// http://localhost:5005/triplicities/:triplicityId?_embed=signs
- -get signs by duality and/or quadruplicity // duality checked// http://localhost:5005/signs/?duality=Masculine&triplicityId=fire1
-   quadruplicity //checked // http://localhost:5005/signs/?quadruplicity=Cardinal&triplicityId=fire1
+// -get signs by duality and/or quadruplicity // duality checked// http://localhost:5005/signs/?duality=Masculine&triplicityId=fire1
+//   quadruplicity //checked // http://localhost:5005/signs/?quadruplicity=Cardinal&triplicityId=fire1
  -get a single sign //checked// http://localhost:5005/signs/:signId
-   -get all users //checked// http://localhost:5005/users
- -get a single user //checked// http://localhost:5005/users/:userId
- -get all favorites //checked// http://localhost:5005/favorites
- -get favorites by user //checked// http://localhost:5005/favorites?userId=ab111112
- -post -add favorite body,  userId signId //checked// http://localhost:5005/favorites
- -delete favorites //checked// http://localhost:5005/favorites/:favoriteId
+ ////  -get all users //checked// http://localhost:5005/users
+//// -get a single user //checked// http://localhost:5005/users/:userId
+//// -get all favorites //checked// http://localhost:5005/favorites
+//// -get favorites by user //checked// http://localhost:5005/favorites?userId=ab111112
+ ////-post -add favorite body,  signId //checked// http://localhost:5005/favorites
+ ////-delete favorites //checked// http://localhost:5005/favorites/:favoriteId
  -get all comments //checked// http://localhost:5005/comments
  -get comments by sign //checked// http://localhost:5005/comments/?signId=1aries
- -get comments by user //checked//http://localhost:5005/comments/?userId=ab111112
- -post create comment body, userId signId text //checked// http://localhost:5005/comments
- -patch edit comment // checked//http://localhost:5005/comments/:commentId
+//// -get comments by user //checked//http://localhost:5005/comments/?userId=ab111112
+ -post create comment body, signName text //comments//checked// http://localhost:5005/comments
+ -patch edit comment //body, text // checked//http://localhost:5005/comments/:commentId
  -delete comment //checked// http://localhost:5005/comments/:commentId
 */
